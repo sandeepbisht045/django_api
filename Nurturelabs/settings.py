5111c0b93d983e4d34ce9b57d54eb927565e17dd
sandeepbisht045 <sandeepbisht045@gmail.com>
2021-10-30 12:01:04 +0530

    commit 4th

--- Nurturelabs/settings.py
+++ Nurturelabs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2j&t6^=96l)+scngu7^pfq3h7falmfq=zwjeto2*+mtz!0q$5f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost","https://nurturelabsapi.herokuapp.com/"]
 
 
 # Application definition
